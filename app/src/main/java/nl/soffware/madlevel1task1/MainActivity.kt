e6a05ed8c57e006df026e32c7991e1a46743be4c
David Soff <david@soff.nl>
2020-08-31 20:49:18 +0200

    Finish diceroller app

--- app/src/main/java/nl/soffware/madlevel1task1/MainActivity.kt
+++ app/src/main/java/nl/soffware/madlevel1task1/MainActivity.kt
@@ -2,7 +2,10 @@ package nl.soffware.madlevel1task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import nl.soffware.madlevel1task1.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,15 +13,75 @@ class MainActivity : AppCompatActivity() {
     private var currentThrow: Int = 1
     private var lastThrow: Int = 1
 
+    private val LOWER = 0
+    private val EQUAL = 1
+    private val HIGHER = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        updateUI()
+        binding.btnLower.setOnClickListener {
+            handleClick(LOWER)
+        }
+        binding.btnEquals.setOnClickListener {
+            handleClick(EQUAL)
+        }
+        binding.btnHigher.setOnClickListener {
+            handleClick(HIGHER)
+        }
     }
 
-    fun updateUI(){
-        val imageName = String.format("dice%1", currentThrow)
+    private fun handleClick(action: Int) {
+        rollDice()
+        animateDice(action)
+    }
+
+    private fun showMessage(action: Int) {
+        when {
+            action == LOWER && lastThrow > currentThrow -> showSuccess()
+            action == EQUAL && lastThrow == currentThrow -> showSuccess()
+            action == HIGHER && lastThrow < currentThrow -> showSuccess()
+            else -> showFailure()
+        }
+    }
+
+    private fun showSuccess() {
+        Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+    }
+
+    private fun showFailure() {
+        Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+    }
+
+    private fun rollDice() {
+        lastThrow = currentThrow
+        currentThrow = (1..6).random()
+    }
+
+    private fun animateDice(action: Int) {
+        updateUI()
+        val timer = object: CountDownTimer(1000,30) {
+            override fun onTick(milisUntillFinished: Long) {
+                setDiceImageTo((1..6).random())
+            }
+            override fun onFinish() {
+                updateUI()
+                showMessage(action)
+            }
+        }
+        timer.start()
+    }
+
+    private fun setDiceImageTo(number: Int){
+        val imageName = "dice$number"
         val imageID = resources.getIdentifier(imageName, "drawable", packageName)
         binding.imgDiceView.setImageDrawable(getDrawable(imageID))
+    }
+
+    private fun updateUI(){
+        setDiceImageTo(currentThrow)
         binding.etLastThrow.text = getString(R.string.last_throw, lastThrow)
     }
 }
